82811ab7b7a5102bbbbf9f9fc5bfe5875ad6bd38
datho7561 <davidethompson@me.com>
2017-01-18 19:00:24 -0500

    work on history activity

--- app/src/main/java/com/example/d/culminatingproject/HistoryActivity.java
+++ app/src/main/java/com/example/d/culminatingproject/HistoryActivity.java
@@ -1,7 +1,9 @@
 package com.example.d.culminatingproject;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        // Set up back button ont eh actionbar
+        android.app.ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            // The app icon has been clicked
+            case R.id.home:
+                this.finish();
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/d/culminatingproject/SettingsActivity.java
+++ app/src/main/java/com/example/d/culminatingproject/SettingsActivity.java
@@ -1,8 +1,18 @@
 package com.example.d.culminatingproject;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Madison on 1/15/2017.
  */
 
-public class SettingsActivity {
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
 }
